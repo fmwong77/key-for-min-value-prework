f0c190f683d637e965056fc7079216ec87885f6c
Mei Wong <fongmeiw@gmail.com>
2019-10-09 20:47:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,15 +6,18 @@ def key_for_min_value(name_hash)
     nil
   else
     min_value = nil
+    min_key = nill
     name_hash.collect do |key, value|
       if min_value == nil 
         min_value = value
+        min_key = key
       end
       if value < min_value
         min_value = value
+        min_key = key
       end
     end
   end
   
-  min_value
+  min_key
 end
